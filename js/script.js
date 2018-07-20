25323a335e094d9149eefe98b9caca41109953a6
ethankclam <ethankclam@gmail.com>
2018-07-20 23:59:35 +0800

    update

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@
 $(function (){
     $("#navbarToggle").blur(function (event){
         var screenWidth = window.innerWidth;
-        if (screenWidth < 768 {
+        if (screenWidth < 768) {
             $("#collapsabe-nav").collapse('hide');
-            })
+            }
     });
-});
